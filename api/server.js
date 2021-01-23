7211c16eda29c382d91aa72e46f4bfbf0aef772e
Jacob Morris <jacobthomasmorris@gmail.com>
2021-01-23 12:58:16 -0700

    first commit

--- api/server.js
+++ api/server.js
@@ -4,4 +4,8 @@ const server = express();
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 
+server.listen(5000, ()=>{
+    console.log('server is running');
+})
+
 module.exports = server;
